da6025724eb4448851729e76cbb30ccb27cdd5e4
kpoppel <4174578+kpoppel@users.noreply.github.com>
2021-12-07 00:00:11 +0100

    Update sensor to latest HomeAssistant.

--- custom_components/novafos/sensor.py
+++ custom_components/novafos/sensor.py
@@ -1,7 +1,9 @@
 """Platform for Eloverblik sensor integration."""
 import logging
-from homeassistant.const import VOLUME_CUBIC_METERS
-from homeassistant.helpers.entity import Entity
+from homeassistant.const import (VOLUME_CUBIC_METERS, DEVICE_CLASS_GAS)
+from homeassistant.components.sensor import (SensorEntity, STATE_CLASS_MEASUREMENT, STATE_CLASS_TOTAL,
+                                            STATE_CLASS_TOTAL_INCREASING)
+#from homeassistant.helpers.entity import Entity
 from custom_components.novafos.pynovafos.novafos import Novafos
 from custom_components.novafos.pynovafos.models import TimeSeries
 
@@ -21,7 +23,7 @@ async def async_setup_entry(hass, config, async_add_entities):
     async_add_entities(sensors)
 
 
-class NovafosWater(Entity):
+class NovafosWater(SensorEntity):
     """Representation of a Sensor."""
 
     def __init__(self, name, sensor_type, client, hour=None):
@@ -30,55 +32,42 @@ class NovafosWater(Entity):
         self._data_date = None
         self._data = client
         self._hour = hour
-        self._name = name
         self._sensor_type = sensor_type
 
+        self._attr_name = name
+
+
         if sensor_type == 'hour':
-            self._unique_id = f"{self._data.get_metering_point()}-{hour}"
+            self._attr_unique_id = f"{self._data.get_metering_point()}-{hour}"
         else:
-            self._unique_id = f"{self._data.get_metering_point()}-total"
+            self._attr_unique_id = f"{self._data.get_metering_point()}-total"
+
+        self._attr_native_unit_of_measurement = VOLUME_CUBIC_METERS
+        self._attr_icon = "mdi:water"
+        self._attr_state_class = STATE_CLASS_MEASUREMENT #STATE_CLASS_TOTAL
+        # Only gas can be measured in m3
+        self._attr_device_class = DEVICE_CLASS_GAS
 
     @property
-    def name(self):
-        """Return the name of the sensor."""
-        return self._name
-
-    @property
-    def unique_id(self):
-        """The unique id of the sensor."""
-        return self._unique_id
-
-    @property
-    def state(self):
-        """Return the state of the sensor."""
-        _LOGGER.debug(f"Asked for state {self._name} = {self._state}")
-        return self._state
-
-    @property
-    def device_state_attributes(self):
-        """Return state attributes."""
+    def extra_state_attributes(self):
+        """Return extra state attributes."""
         attributes = dict()
         attributes['Metering date'] = self._data_date
         attributes['metering_date'] = self._data_date
-        
         return attributes
-
-    @property
-    def unit_of_measurement(self):
-        """Return the unit of measurement."""
-        return VOLUME_CUBIC_METERS
 
     def update(self):
         """Fetch new state data for the sensor.
         This is the only method that should fetch new data for Home Assistant.
         """
-        self._data.update()        
+        _LOGGER.debug(f"Setting status for {self._attr_name}")
+        self._data.update()
 
         self._data_date = self._data.get_data_date()
 
         if self._sensor_type == 'total':
-            self._state = self._data.get_year_to_date()
+            self._attr_native_value = self._data.get_year_to_date()
         else:
-            self._state = self._data.get_usage_hour(self._hour)
-        _LOGGER.debug(f"Setting status for {self._name} = {self._state} {VOLUME_CUBIC_METERS}")
+            self._attr_native_value = self._data.get_usage_hour(self._hour)
+        _LOGGER.debug(f"Setting status for {self._attr_name} = {self._attr_native_value} {self._attr_native_unit_of_measurement}")
 
